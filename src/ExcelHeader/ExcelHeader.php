fdb6ceafe5fc33450f5a6f467d31cc9de338906d
Mahavir Vataliya <mahavir.vataliya110@gmail.com>
2018-03-13 17:30:50 +0530

    Get All Header From AllSheets

--- src/ExcelHeader/ExcelHeader.php
+++ src/ExcelHeader/ExcelHeader.php
@@ -32,4 +32,35 @@ class ExcelHeader
         }
         return json_encode($rows);
     }
+    public static function getAllSheetExcelHeader($filepath,$rowno=1)
+    {
+        $rows = [];
+        try{
+            $spreadsheet = IOFactory::load($filepath);
+            //$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+            //$spreadsheet = $reader->load($filepath);
+           // $worksheet = $spreadsheet->getActiveSheet();
+            $sheetnames = $spreadsheet->getSheetNames();
+            foreach($sheetnames AS $sheetname)
+            {
+                $worksheet = $spreadsheet->getSheetByName($sheetname);
+                foreach ($worksheet->getRowIterator($rowno,$rowno) AS $row) {
+                    $cellIterator = $row->getCellIterator();
+                    $cellIterator->setIterateOnlyExistingCells(FALSE); // This loops through all cells,
+                    $cells = [];
+                    foreach ($cellIterator as $cell) {
+                        $cells[] = $cell->getValue();
+                    }
+                    $rows[] = $cells;
+                }
+            }
+            return json_encode($rows);
+            } catch (Exception $exception)
+        {
+            die($exception);
+        }
+        return json_encode($rows);
+
+
+    }
 }
